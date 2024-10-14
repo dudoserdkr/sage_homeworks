8de4e1f723fa45c6f482b93d0adecb5025baafb0
dudoserdkr <dudoserdkr@gmail.com>
2024-10-14 22:59:41 +0300

    backup before algo started

--- lab03/algo.py
+++ lab03/algo.py
@@ -1,6 +1,19 @@
 import time
 from math import factorial
 
+
+class FileWriter:
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def write(self, data: dict) -> None:
+        i = 0
+        with open(self.file_name, 'w') as f:
+            for key, value in data.items():
+                i += 1
+                f.write(f'Order: {key}: Count: {value}\n')
+
+
 class LCD:
     def __init__(self, partition: str, partitioned_number: int) -> None:
         self.part = list(map(int, partition.strip().split()))
@@ -43,20 +56,23 @@ if __name__ == '__main__':
     start_time = time.time()
 
     with open("integers_partitions_100.txt", "r") as file:
-        i = 0
         S100 = {}
         for partition in file:
-            i += 1
             lcd = LCD(partition, 100)
             S100[lcd.lcd] = S100.get(lcd.lcd, 0) + lcd.perm_count
-            if i == 100000:
-                break
 
     S100_sorted = dict(sorted(S100.items()))
 
-    with open('output02_S100.txt', 'w') as f:
-        for key, value in S100_sorted.items():
-            f.write(f'Order: {key}; Count: {value}\n')
+    A100_sorted = {}
+    for order, count in S100_sorted.items():
+        if order % 2 == 0:
+            A100_sorted[order] = count // 2
+
+    file_writer = FileWriter("output02_S100.txt")
+    file_writer.write(S100_sorted)
+
+    file_writer.file_name = "output02_A100.txt"
+    file_writer.write(A100_sorted)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
